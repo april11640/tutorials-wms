27137875f39384364670730b137f38153c2f7e69
zhangshaoping <zhangshaoping@qianqiantech.com>
2019-07-02 19:03:48 +0800

    m

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,3 +1,17 @@
+CREATE TABLE `undo_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+
 CREATE TABLE `inventory` (
   `coord_x` bigint(20) unsigned NOT NULL,
   `coord_y` bigint(20) unsigned NOT NULL,
@@ -7,7 +21,7 @@ CREATE TABLE `inventory` (
   `allocation_count` int(11) NOT NULL,
   `last_modification_time` timestamp(6) NOT NULL,
   PRIMARY KEY (`coord_x`,`coord_y`,`coord_z`,`goods_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE `inventory_log` (
@@ -24,4 +38,32 @@ CREATE TABLE `inventory_log` (
   `change_count` int(11) NOT NULL,
   `creation_time` timestamp(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2854 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=2854 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `purchase_order` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `creation_time` timestamp(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `purchase_order_goods` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `order_id` bigint(20) unsigned NOT NULL,
+  `goods_id` bigint(20) unsigned NOT NULL,
+  `count` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `purchase_order_allocation` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `order_id` bigint(20) unsigned NOT NULL,
+  `coord_x` bigint(20) unsigned NOT NULL,
+  `coord_y` bigint(20) unsigned NOT NULL,
+  `coord_z` bigint(20) unsigned NOT NULL,
+  `goods_id` bigint(20) unsigned NOT NULL,
+  `count` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
